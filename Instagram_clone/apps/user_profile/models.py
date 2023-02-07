e175a5a0c65184f57c7b78dbf75f7351a495cd5d
iamjatinyadav <jatinyadav0000@gmail.com>
2023-02-07 16:24:42 +0530

    make changes

--- Instagram_clone/apps/user_profile/models.py
+++ Instagram_clone/apps/user_profile/models.py
@@ -68,6 +68,23 @@ class UserProfile(TimeStampedModel):
         return str(self.user) + "Add detail"
 
 
+def user_post_directory_path(instance, filename):
+    return 'UserPost/{0}/{1}'.format(instance.user.userpersonal.uniquename, filename)
+
+
+class Post(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_post")
+    image = models.ImageField(upload_to=user_post_directory_path)
+    image_bio = models.CharField(max_length=200, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "UserPost"
+        verbose_name_plural = "UserPosts"
+
+    def __str__(self):
+        return str(self.user) + " add a picture " + self.pk
+
+
 
 
 
